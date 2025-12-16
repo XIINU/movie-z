e3225a32a581af2a4c462b7c044219207b89cb2e
xiinu <zinu.zinesh47@gmail.com>
2025-12-16 14:24:31 +0530

    services with api calls

---
+++ services/api.ts
@@ -0,0 +1,59 @@
+// export const TMDB_CONFIG = {
+//   BASE_URL: "https://api.themoviedb.org/3",
+//   API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+//   headers: {
+//     accept: "application/json",
+//     Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+//   },
+// };
+
+// export const fetchMovies = async ({
+//   query,
+// }: {
+//   query: string;
+// }): Promise<Movie[]> => {
+//   const endpoint = query
+//     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+//     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+//   // console.log("TMDB KEY:", process.env.EXPO_PUBLIC_MOVIE_API_KEY);
+
+//   const response = await fetch(endpoint, {
+//     method: "GET",
+//     headers: TMDB_CONFIG.headers,
+//   });
+
+//   if (!response.ok) {
+//     throw new Error(`Failed to fetch movies: ${response.statusText}`);
+//   }
+
+//   const data = await response.json();
+//   return data.results;
+// };
+
+import { Movie } from "@/types/types";
+
+type DummyResponse = {
+  products: Movie[];
+};
+
+export const fetchMovies = async ({
+  query,
+}: {
+  query: string;
+}): Promise<Movie[]> => {
+  const endpoint = query
+    ? `https://dummyjson.com/products/search?q=${encodeURIComponent(query)}`
+    : `https://dummyjson.com/products`;
+
+  const response = await fetch(endpoint);
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch data: ${response.status} ${response.statusText}`
+    );
+  }
+
+  const data: DummyResponse = await response.json();
+  return data.products;
+};
---
+++ types/types.ts
@@ -0,0 +1,7 @@
+export type Movie = {
+  id: number;
+  title: string;
+  description: string;
+  thumbnail: string;
+  rating: number;
+};
